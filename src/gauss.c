86a827a93f67f68bd66002ca9b8d783055691cd7
karolinaaklimczak <01206776@pw.edu.pl>
2026-01-04 16:53:26 +0100

    Add Gaussian elimination

---
+++ src/gauss.c
@@ -0,0 +1,12 @@
+#include "gauss.h"
+void gauss_elimination(int n, double A[n][n], double b[n]) {
+    for (int k = 0; k < n - 1; k++) {
+        for (int i = k + 1; i < n; i++) {
+            double factor = A[i][k] / A[k][k];
+            for (int j = k; j < n; j++) {
+                A[i][j] -= factor * A[k][j];
+            }
+            b[i] -= factor * b[k];
+        }
+    }
+}
